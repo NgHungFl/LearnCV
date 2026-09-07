57fbdc40082271323c9fede230b5b2e46bddae30
NgHungFl <nghung.lxag.2005@gmail.com>
2026-09-07 23:28:45 +0700

    fix

--- week2/hw1.py
+++ week2/hw1.py
@@ -2,10 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
 
-img = cv.imread('img/lenna.jpg')
+img = cv.imread('img/messi.jpg')
 
 cv.imshow('Original Image', img)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
-# end assaignment 1
